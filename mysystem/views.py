faa239d939a2c6ba5b5ccdf399cfb3c94a0c5939
denffer <denffer18@gmail.com>
2016-12-21 21:35:04 +0800

    Version 1.1 (Prototype)

--- mysystem/views.py
+++ mysystem/views.py
@@ -3,31 +3,35 @@ import os, time, re
 from stat import  *
 
 # Create your views here.
-def show(request):
+def index(request):
+    """ show background.txt """
+    return render(request, 'background.html')
+
+def show(request, assignment_index):
     # provide information last modified time
-    root_dir = "/usr/local/class/1052sp/assignment1/"
-    status_list = []
-    for dirpath, dir_list, file_list in os.walk(root_dir):
-        print "Walking into directory: " + str(dirpath)
-        if len(file_list) > 0:
-            student_id = re.search(r"(u[0-9]+)", dirpath).group(1)
-            "Found student:", student_id
+    root_dir = "/usr/local/class/1052sp/assignment" + str(assignment_index) + "/"
+    if os.path.exists(root_dir):
+        status_list = []
+        for dirpath, dir_list, file_list in os.walk(root_dir):
+            print "Walking into directory: " + str(dirpath)
+            if len(file_list) > 0:
+                student_id = re.search(r"(u[0-9]+)", dirpath).group(1)
+                "Found student:", student_id
 
-            for f in file_list:
-                if str(f) == "score.txt":
-                    st = os.stat(dirpath + "/" + f)
-                    t = time.asctime(time.localtime(st[ST_MTIME]))
-                    student = Student(student_id, True, t)
+                for f in file_list:
+                    if str(f) == "score.txt":
+                        st = os.stat(dirpath + "/" + f)
+                        t = time.asctime(time.localtime(st[ST_MTIME]))
+                        student = Student(student_id, True, t)
 
-                    status_list.append(student)
-        else:
-            print "No file is found."
+                        status_list.append(student)
+            else:
+                pass
+                #print "No file is found."
+        return render(request, 'show.html', {'status_list': status_list})
+    else:
+        return render(request, 'coming_soon.html')
 
-                    #  with open(dirpath +"/"+ f) as file:
-                    #      corpus.append(file.read())
-
-    #print info_dicts
-    return render(request, 'show.html', {'status_list': status_list})
 
 class Student(object):
 
